90444042d69a6540b3cf1b26114af19cb96037e9
Bastiche <bastien.velitchkine@live.fr>
2021-10-31 12:11:34 +0100

    Docstrings in variables

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
+/**
+ * A function that returns every global variable of the project.
+ * @returns {Object} the object that contains the global variables for this project
+ */
 function globalVariables() {
     return Object.freeze({
       "MAIL": {
@@ -5,8 +9,8 @@ function globalVariables() {
         },
       "SPREADSHEET": {
         "spreadsheetId": "👉 Your Spreadsheet Id Here 👈 ",
-        "acquisitionsSheet": "Acquisitions",
-        "fundRaisingsSheet": "Fund Raisings"
+        "acquisitionsSheet": "Acquisitions", // The name of the sheet in your spreadsheet that contains acquisitions' signals
+        "fundRaisingsSheet": "Fund Raisings" // The name of the sheet in your spreadsheet that contains fund raisings' signals
       }
     });
   };
