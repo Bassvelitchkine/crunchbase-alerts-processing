d81a398a7339e796f788de44f2bc56336ea08fe4
Bastiche <bastien.velitchkine@live.fr>
2021-10-31 12:09:55 +0100

    Docstrings in HtmlParser

--- HtmlParser.js
+++ HtmlParser.js
@@ -1,7 +1,11 @@
+/**
+ * Stricto Sensu it's a function but conceptually, we use it like a class. This class contains a few variables but most importantly methods to parse the html content of our crunchbase alert emails.
+ * @returns {Object} the frozen object with all the methods related to crunchbase emails' html parsing.
+ */
 function HtmlParser() {
   
-    // Use https://regex101.com/ to test the regular expressions
-    // you don't understand on a downloaded crunchbase email alert.
+    // Use https://regex101.com/ to test the regular expressions you
+    // don't understand on a downloaded crunchbase email alert.
     const localVariables = {
       "REGEX": {
         "aTag": /<a(.|\s)*?<\/a>/g,
@@ -11,6 +15,14 @@ function HtmlParser() {
       }
     };
   
+    /**
+     * A function that extracts all the relevant data from <a> tags in an email html content in a structured way.
+     * @param {String} message a single email's html content
+     * @returns {Array} a 2D-Array where each line is data on an <a>...</a> tag extracted from the email. The first element is the url of the a tag, the second one is the associated text.
+     * 
+     * _extractATagsData("<html>some html content with a bunch of a tags inside</html>")
+     * // => [["https://first.extracted.url", "Text associated to the first url"], ["https://second.extracted.url", "Text associated to the second url"], ["https://crunchabse.search.url", "GO TO MY SEARCHES"], ["https://fourth.extracted.url", "Text associated to the fourth url"], ...];
+     */
     function _extractATagsData(message){
       const regex = localVariables["REGEX"];
   
@@ -37,6 +49,14 @@ function HtmlParser() {
       }).filter(elem => elem); // Some <a> did not have urls or text. They need to be removed.
     };
   
+    /**
+     * Some of the a tags extracted from the emails correspond to the Crunchbase Search the events originate from. Explanation: The user designed searches in Crunchbase with a few criteria and set up email alerts fr those searches. The user receives daily email alerts whenever there's a change in his crunchbase search. In those emails, there's always a link to the actual crunchbase search that triggered the email alert.
+     * @param {Array} aTagsData the 2D-Array as returned by '_extractATagsData'
+     * @returns {Array} the array of indexes of elements in the input that correspond to a link to a crunchbase search url.
+     * 
+     * _findCrunchSearchIndexes([["https://first.extracted.url", "Text associated to the first url"], ["https://second.extracted.url", "Text associated to the second url"], ["https://crunchabse.search.url", "GO TO MY SEARCHES"], ["https://fourth.extracted.url", "Text associated to the fourth url"], ..., ["https://crunchabse.second.search.url", "GO TO MY SEARCHES"], ...])
+     * // => [2, 5, ...];
+     */
     function _findCrunchSearchIndexes(aTagsData){
       let result = [];
       aTagsData.forEach((aTagData, index) => {
@@ -46,6 +66,14 @@ function HtmlParser() {
       return result;
     };
   
+    /**
+     * A function that appends the crunchbase search origin to every event extracted from an email. Note that each event necessarily appears before the link to the search it originates from, like so: [event from the first search, other event from the first search, link to the first crunchbase search, events from the second search, link to the second crunchbase search, ..., link to the last crunchbase search mentioned in the email, a bunch of other useless links].
+     * @param {Array} aTagsData the 2D-Array as returned by '_extractATagsData'
+     * @returns {Array} events from the input array whith their corresponding crunchbase search url (see example below).
+     * 
+     * _assignCrunchSearchOrigin([["https://first.extracted.url", "Text associated to the first url"], ["https://second.extracted.url", "Text associated to the second url"], ["https://crunchabse.first.search.url", "GO TO MY SEARCHES"], ["https://fourth.extracted.url", "Text associated to the fourth url"], ..., ["https://crunchabse.second.search.url", "GO TO MY SEARCHES"], ...])
+     * // => [["https://crunchabse.first.search.url", "https://first.extracted.url", "Text associated to the first url"], ["https://crunchabse.first.search.url", "https://second.extracted.url", "Text associated to the second url"], ["https://crunchabse.second.search.url", "https://fourth.extracted.url", "Text associated to the fourth url"], ... ["https://crunchabse.last.search.url", "https://last.event.url", "Text associated to the last evetnt"]];
+     */
     function _assignCrunchSearchOrigin(aTagsData){
       const crunchSearchIndexes = _findCrunchSearchIndexes(aTagsData);
       let result = [];
@@ -65,6 +93,14 @@ function HtmlParser() {
       return result;
     };
   
+    /**
+     * A function that extract every events mentioned in all unread messages in a structured way. See example below.
+     * @param {Array} idsAndMessages an array of objects, each one corresponding to a single email. It has the email id as key and its html content as value.
+     * @returns {Array} the same array as in the input but instead of the html content as objects' values, it has the extracted <a> tags' data as returned by '_assignCrunchSearchOrigin' applied to '_extractATagsData'
+     * 
+     * extractBusinessData([{"56386589": "<html>...email content</html>"}, {"6098732": "<html> Some other html content</html>"}, ...]);
+     * // => [{"56386589": [["https://crunchbase.first.search.url.from.first.email", "https://crunchabse.first.event.url.from.first.mail", "First event description from first email"]]}, {"6098732": [["https://crunchbase.first.search.url.from.second.email", "https://crunchabse.first.event.url.from.second.mail", "First event description from second email"]]}, ...];
+     */
     function extractBusinessData(idsAndMessages){
       return idsAndMessages.map(message => {
         for ([messageId, text] of Object.entries(message)){
